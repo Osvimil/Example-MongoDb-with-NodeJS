fbb873acca85f66cf851cdc4771f994b1726a59f
oswaldoadidas <oswaldoadidas@hotmail.com>
2016-06-24 11:32:05 -0500

    codigo comentareado

--- mongofile.js
+++ mongofile.js
@@ -1,17 +1,23 @@
+//MongoDB variable que requiere uso de mongob
 var mongodb = require('mongodb');
 
+//variable que enlaza el requerimiento MongoDb con otra
+//variable
 var MongoClient = mongodb.MongoClient;
 
+// url de direccionamiento
 var url = 'mongodb://localhost:27017/fruits';
+
 
 MongoClient.connect(url,function(err,db){
 	if(err){
+		//muestra mensaje si hay problemas al conectar
 		console.log(err);
 
 	}else{
+		// muestra conexion exitosa y cierra la conexion
 		console.log('Connected to ',url);
 		db.close();
-
 	}
 
 
